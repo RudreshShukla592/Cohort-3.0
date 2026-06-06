c924db7d8681a2ecacd9806d5e9ef5a9709a6f0f
Ansh <rudreshshukla592@gmail.com>
2026-06-06 09:31:14 +0530

    JS phase-1 practice

--- JS-sessions/Phase-1/script.js
+++ JS-sessions/Phase-1/script.js
@@ -1,7 +1,96 @@
-console.log("hola js");
-console.warn("WArning!! ")
+/* 
+Console & Basics
+*/
 
-var a = "hello Worlds"
+// // 1
+// console.log("hola js");
 
-console.log(a);
+// // 2
+// console.log("Rudresh",18,"Lucknow");
 
+// // 3
+// console.warn("WArning!! ")
+
+// // 4
+// console.error("Error!!!");
+
+// // 5
+// console.table([1,2,3,4,5])
+
+/* 
+Variables
+*/
+
+// // 1
+// const studentName = "Rudresh"
+
+// // 2
+// const age =18;
+// console.log(age);
+
+// // 3
+// let a=20;
+// let b=30;
+
+// [a,b] = [b,a]
+
+// console.log(a,b);
+
+// // 4
+// const pie = Math.PI
+// console.log(pie);
+
+// // 5
+// let c;
+// console.log(c);
+
+/* 
+Data Types
+*/
+
+// // 4
+// let empty = null
+// console.log(typeof empty);
+
+// // 5
+// let bigNum = 50n;
+// console.log(typeof bigNum);
+
+/* 
+Type Conversion & Coercion
+*/
+
+
+
+/* 
+Conditionals
+*/
+
+// // 5
+// let a=233;
+// let b=35;
+// let c=91;
+
+// if(a> b && a>c) console.log("a is greatest");
+// else if(b>a && b>c) console.log("b is greatest");
+// else console.log("c is greatest");
+
+// // 6
+// let year=2003;
+
+// if((year%400 === 0)||(year%4===0 && year%100 !== 0)) console.log("leap year");
+// else console.log("not leap year");
+
+// // 9
+// let char = "b";
+
+// if(char = )
+
+
+/* 
+Conditionals
+*/
+
+
+
+    
