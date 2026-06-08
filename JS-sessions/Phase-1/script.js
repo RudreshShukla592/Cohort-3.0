8a48c8662b39134a9c5e2f5811fa5578ac4f6faa
Ansh <rudreshshukla592@gmail.com>
2026-06-08 14:42:54 +0530

    JS phase-1 practice

--- JS-sessions/Phase-1/script.js
+++ JS-sessions/Phase-1/script.js
@@ -2,59 +2,36 @@
 Console & Basics
 */
 
-// 1
-console.log("hola js");
+// // 1
+// console.log("hola js");
 
-// 2
-console.log("Rudresh",18,"Lucknow");
+// // 2
+// console.log("Rudresh",18,"Lucknow");
 
-// 3
-console.warn("WArning!! ")
+// // 3
+// console.warn("WArning!! ")
 
-// 4
-console.error("Error!!!");
+// // 4
+// console.error("Error!!!");
 
-// 5
-console.table([1,2,3,4,5])
+// // 5
+// console.table([1,2,3,4,5])
 
-/* 
-Variables
-*/
+// /* 
+// Variables
+// */
 
-// 1
-const studentName = "Rudresh"
+// // 1
+// const studentName = "Rudresh"
 
-// 2
-const age =18;
-console.log(age);
+// // 2
+// const age =18;
+// console.log(age);
 
-// 3
-let a=20;
-let b=30;
+// // 3
+// let a=20;
+// let b=30;
 
-[a,b] = [b,a]
-
-console.log(a,b);
-
-// 4
-const pie = Math.PI
-console.log(pie);
-
-// 5
-let c;
-console.log(c);
-
-/* 
-Data Types
-*/
-
-// 4
-let empty = null
-console.log(typeof empty);
-
-// 5
-let bigNum = 50n;
-console.log(typeof bigNum);
 
 /* 
 Type Conversion & Coercion
@@ -67,30 +44,40 @@ Conditionals
 */
 
 // 5
-let a=233;
-let b=35;
-let c=91;
+// let a=233;
+// let b=35;
+// let c=91;
 
-if(a> b && a>c) console.log("a is greatest");
-else if(b>a && b>c) console.log("b is greatest");
-else console.log("c is greatest");
+// if(a> b && a>c) console.log("a is greatest");
+// else if(b>a && b>c) console.log("b is greatest");
+// else console.log("c is greatest");
 
-// 6
-let year=2003;
+// // 6
+// let year=2003;
 
-if((year%400 === 0)||(year%4===0 && year%100 !== 0)) console.log("leap year");
-else console.log("not leap year");
+// if((year%400 === 0)||(year%4===0 && year%100 !== 0)) console.log("leap year");
+// else console.log("not leap year");
 
-// 9
-let char = "b";
+// // 9
+// let char = "b";
 
-if(char = )
+// if(char = )
 
 
 /* 
 Conditionals
 */
 
+/*
+Loops
+*/
 
-
+let n= Number(prompt("enter number"))
+let i=1
+while(i<=10){
+    console.log(n*i);
+    i++
     
+}
+
+
