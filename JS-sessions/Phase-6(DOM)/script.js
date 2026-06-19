098f3940e1e85badff48334f2dd34f5b374ba2bb
Ansh <rudreshshukla592@gmail.com>
2026-06-19 12:09:05 +0530

    Bulb making

--- JS-sessions/Phase-6(DOM)/script.js
+++ JS-sessions/Phase-6(DOM)/script.js
@@ -1,11 +1,6 @@
-const h1 =
-document.getElementById("title");
+let div=document.querySelector(".bulb")
+let btn = document.querySelector("button")
 
-h1.style.color='red'
-h1.style.fontSize="50px"
-
-console.log(h1.classList.contains("h1"));
-
-h1.classList.replace("h1","heading")
-
-console.log(h1.classList.contains("heading"));
+btn.addEventListener("click",()=>{
+    div.classList.toggle("yellow")
+})
