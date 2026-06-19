a1b710beabf9caccfcbc2c7f2527e2cd178adcb6
Ansh <rudreshshukla592@gmail.com>
2026-06-19 19:19:18 +0530

    Bulb finished

--- JS-sessions/Phase-6(DOM)/script.js
+++ JS-sessions/Phase-6(DOM)/script.js
@@ -1,6 +1,14 @@
 let div=document.querySelector(".bulb")
 let btn = document.querySelector("button")
 
+let flag= true
 btn.addEventListener("click",()=>{
     div.classList.toggle("yellow")
+    if(flag){
+         btn.textContent="OFF"
+         flag=false
+    }else{
+         btn.textContent="ON"
+         flag=true
+    }
 })
