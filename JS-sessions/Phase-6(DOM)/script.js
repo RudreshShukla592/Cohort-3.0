54a00c9bdb848b6a7a14badb4d7d963a76584a01
Ansh <rudreshshukla592@gmail.com>
2026-06-19 11:55:10 +0530

    Initial commit

--- JS-sessions/Phase-6(DOM)/script.js
+++ JS-sessions/Phase-6(DOM)/script.js
@@ -1,6 +1,11 @@
-const heading =
+const h1 =
 document.getElementById("title");
 
-console.dir(heading)
+h1.style.color='red'
+h1.style.fontSize="50px"
 
-console.log(document.getElementsByTagName("p"));
+console.log(h1.classList.contains("h1"));
+
+h1.classList.replace("h1","heading")
+
+console.log(h1.classList.contains("heading"));
