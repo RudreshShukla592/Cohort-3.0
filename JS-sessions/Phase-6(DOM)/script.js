33512a6945d2ca1d81caef1867904bc4103f4305
Ansh <rudreshshukla592@gmail.com>
2026-06-20 12:11:47 +0530

    DOM-3

--- JS-sessions/Phase-6(DOM)/script.js
+++ JS-sessions/Phase-6(DOM)/script.js
@@ -1,6 +1,7 @@
 let div=document.querySelector(".bulb")
 let btn = document.querySelector("button")
-
+let btn2 = document.querySelector(".val")
+let inp = document.querySelector("input")
 
 btn.addEventListener("click",()=>{
     if(div.classList.toggle("yellow")){
@@ -8,4 +9,12 @@ btn.addEventListener("click",()=>{
     }else{
          btn.textContent="ON"
     }
+})
+
+let attr = div.getAttribute("class")
+console.log(attr);
+
+btn2.addEventListener("click",()=>{
+    console.log(inp.value);
+    
 })
