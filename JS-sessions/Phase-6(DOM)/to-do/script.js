8f993e2889ab9c508d58d85102e4831ce3c47173
Ansh <rudreshshukla592@gmail.com>
2026-06-20 19:44:01 +0530

    Starting todo

--- JS-sessions/Phase-6(DOM)/to-do/script.js
+++ JS-sessions/Phase-6(DOM)/to-do/script.js
@@ -1,15 +1,16 @@
 let inp =  document.querySelector("input");
 let addBtn = document.querySelector("#add")
 let todo = document.querySelector(".todo")
+
+let h3 = document.createElement("h3")
+h3.classList.add("h3")
 addBtn.addEventListener("click",()=>{
     const val= inp.value
+    h3.textContent=val;
+
     if(val.trim() === "") return
-    todo.innerHTML+=`<div class="li"> 
-                <h3>${val}</h3>
-                <div>
-                   <button class="btn">Edit</button>
-                   <button class="btn">Delete</button>
-                </div>
-            </div> `
+
+    todo.prepend(h3)
+
     inp.value=""    
 })
