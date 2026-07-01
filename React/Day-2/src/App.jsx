b0b29a22e7d9bde4de3031b2f08dc59b2d77179f
Ansh <rudreshshukla592@gmail.com>
2026-07-01 18:04:49 +0530

    react day 2 completed

--- React/Day-2/src/App.jsx
+++ React/Day-2/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import About from "./About"
 
 
 const App = () => {
+
+  const [count, setcount] = useState(0)
+  
+  let increase = ()=>{
+    setcount(prev => prev + 1)
+    setcount(prev => prev + 1)
+    setcount(prev => prev + 1)
+  }
+  let decrease = ()=>{
+    setcount(prev => prev - 1)
+  }
+
   return (
     <div>
-      <h1>hello fam!!! and world</h1>
+      <h1>count - {count}</h1> 
+      <button onClick={increase}>Increment</button>
+      <button onClick={decrease}>Decrement</button>
       <About name="natwaaaaa2" age={19} />
     </div>
   )
