e50a1338e31b8cb7ba539a17098bf70925c9ea85
Ansh <rudreshshukla592@gmail.com>
2026-07-02 21:34:29 +0530

    project fintrack

--- ExpenseTracker/js/script.js
+++ ExpenseTracker/js/script.js
@@ -1,19 +1,22 @@
-let themeSlider = document.querySelector("#theme-toggle")
+let themeSlider = document.querySelector("#theme-toggle");
 let html = document.querySelector("html");
 
-themeSlider.addEventListener("change",()=>{
-  let theme = html.getAttribute("data-theme");
-  if (theme === "dark") {
-    html.setAttribute("data-theme", "light");
-    
-    localStorage.setItem("theme", "light");
-  } else {
+themeSlider.addEventListener("change", () => {
+  if (themeSlider.checked) {
     html.setAttribute("data-theme", "dark");
-   
     localStorage.setItem("theme", "dark");
+  } else {
+    html.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
   }
-})
+});
 let savedTheme = localStorage.getItem("theme");
-if(savedTheme){
-    html.setAttribute("data-theme", savedTheme);
+if (savedTheme) {
+  html.setAttribute("data-theme", savedTheme);
+
+  if (savedTheme === "dark") {
+    themeSlider.checked = true;
+  } else {
+    themeSlider.checked = false;
+  }
 }
