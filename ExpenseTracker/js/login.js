2ab8f215c3b588022a00baa2108070a69e0ff0b4
Ansh <rudreshshukla592@gmail.com>
2026-07-02 23:46:07 +0530

    project login and register done

---
+++ ExpenseTracker/js/login.js
@@ -0,0 +1,32 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let username = e.target[0].value.trim();
+  let password = e.target[1].value.trim();
+
+  if (username === "" || password === "") {
+    alert("Username or Password cannot be empty");
+    return;
+  }
+
+  let savedUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+  let savedName = savedUsers.find((user) => user.username === username);
+  if (savedName) {
+    if (savedName.password === password) {
+      let userLogged = savedName;
+    } else {
+      alert("Wrong Password");
+      form.reset();
+      return;
+    }
+  } else {
+    alert("User not found");
+    form.reset();
+    return;
+  }
+  localStorage.setItem("currentUser", JSON.stringify(userLogged));
+  form.reset();
+  location.href = "./index.html";
+});
