8256b3df8906f20fa70c07389c23a50b57660aba
Ansh <rudreshshukla592@gmail.com>
2026-07-24 22:32:01 +0530

    build counter with redux

--- Redux_for_Hackathon/Phase1/src/App.jsx
+++ Redux_for_Hackathon/Phase1/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByinput } from "./redux/feature/counterSlice";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  // for work to done
+  const dispatch = useDispatch();
 
-export default App
+  // to show the work on ui which is now done
+  const count = useSelector((state) => state.counter.value);
+
+  const [num, setnum] = useState(5)
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+
+      <div className="btn-group">
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </button>
+        <input 
+        type="text"
+        value={num} 
+        onChange={(e)=> setnum(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            dispatch(incrementByinput(Number(num)));
+          }}
+        >
+          incrementByinput
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
