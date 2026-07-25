2208d34839c03abcb4368d40abec547e316931bb
Ansh <rudreshshukla592@gmail.com>
2026-07-25 13:02:52 +0530

    redux prj completed

--- Redux_for_Hackathon/Phase2_Prj/src/components/StudentList.jsx
+++ Redux_for_Hackathon/Phase2_Prj/src/components/StudentList.jsx
@@ -1,5 +1,6 @@
 import StudentCard from "./StudentCard";
 import { useSelector } from "react-redux";
+import { Users } from "lucide-react";
 
 const StudentList = () => {
   const students = useSelector((state) => state.students.students);
@@ -9,13 +10,30 @@ const StudentList = () => {
       <div className="mb-4 flex items-center justify-between">
         <h2 className="text-2xl font-semibold">Students</h2>
 
-        <p className="text-sm text-slate-500">Showing {students.length} students</p>
+        <p className="text-sm text-slate-500">
+          Showing {students.length} students
+        </p>
       </div>
 
       <div className="space-y-4">
-        {students.map((student) => (
-          <StudentCard key={student.id} student={student} />
-        ))}
+        {students.length > 0 ? (
+          students.map((student) => (
+            <StudentCard key={student.id} student={student} />
+          ))
+        ) : (
+          <div className="flex h-52 items-center justify-center rounded-2xl border-2 border-dashed border-slate-300 bg-white">
+            <div className="text-center">
+                <Users className="mx-auto mb-3 h-12 w-12 text-slate-400" />
+                
+              <h3 className="text-lg font-semibold text-slate-700">
+                No Students Found
+              </h3>
+              <p className="mt-2 text-sm text-slate-500">
+                Add your first student using the form above.
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
