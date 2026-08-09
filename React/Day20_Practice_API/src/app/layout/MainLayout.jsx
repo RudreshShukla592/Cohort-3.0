1695e872f39a3df21e8fb6e02f320c151e7b3481
Ansh <rudreshshukla592@gmail.com>
2026-08-09 21:57:25 +0530

    react practice

--- React/Day20_Practice_API/src/app/layout/MainLayout.jsx
+++ React/Day20_Practice_API/src/app/layout/MainLayout.jsx
@@ -4,9 +4,14 @@ import Navbar from '../../shared/ui/components/Navbar'
 
 const MainLayout = () => {
   return (
-    <div>
-        <Navbar/>
-        <Outlet/>
+    <div className="min-h-screen bg-gray-950 text-white">
+
+      <Navbar />
+
+      <main className="max-w-7xl mx-auto px-6 py-8">
+        <Outlet />
+      </main>
+
     </div>
   )
 }
