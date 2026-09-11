a613afd64645f643c2ac70289869944facd2d462
Ansh <rudreshshukla592@gmail.com>
2026-09-11 11:39:45 +0530

    auth integration

---
+++ Backend/Day13_Auth_Implementation/backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+app.use(
+  cors({
+    origin: " http://localhost:5173/",
+  }),
+);
+
+export default app;
