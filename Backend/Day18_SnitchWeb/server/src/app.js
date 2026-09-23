92e9525a0c661a4b798013acdb71a831cbdcc176
Ansh <rudreshshukla592@gmail.com>
2026-09-23 10:03:50 +0530

    making snitch in mern

--- Backend/Day18_SnitchWeb/server/src/app.js
+++ Backend/Day18_SnitchWeb/server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRouter from "./routes/auth.routes.js"
+import productsRouter from "./routes/products.routes.js"
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -7,5 +8,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
+app.use("/api/products",productsRouter)
 
 export default app
